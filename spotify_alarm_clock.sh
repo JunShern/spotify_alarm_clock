9d9c39abdd0af7a356e4ce728ac2c33ed74b33d7
Chan Jun Shern <chanjunshern@gmail.com>
2018-01-07 00:05:04 +0000

    Change to datetime handling in webserver

--- spotify_alarm_clock.sh
+++ spotify_alarm_clock.sh
@@ -4,15 +4,12 @@ unix_time=$(date +%s -d "$1")
 if [ -z "$1" ]; then
     echo "No time argument given!"
 fi
-# If the time has already passed today, set for tomorrow
-DESIRED=$((`date +%s -d "$unix_time"`))
-NOW=$((`date +%s`))
-if [ $DESIRED -lt $NOW ]; then
-    DESIRED=$((`date +%s -d "$1"` + 24*60*60))
-fi
-sudo rtcwake -m mem -t $DESIRED> /dev/null
 
-sleep 5 
+sudo rtcwake -m mem -t $1
+
+sleep 5
+sudo service network-manager restart
+sleep 10
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
     # Connected to wifi; use Spotify
     echo "Starting Spotify..."
@@ -26,9 +23,9 @@ else
     do
         (speaker-test -t sine -f 1000 >/dev/null)& pid=$! ; sleep 0.8s ; kill -9 $pid
         sleep 0.2
-        (speaker-test -t sine -f 2000 >/dev/null)& pid=$! ; sleep 0.8s ; kill -9 $pid
+        (speaker-test -t sine -f 1400 >/dev/null)& pid=$! ; sleep 0.8s ; kill -9 $pid
         sleep 0.2
-        (speaker-test -t sine -f 4000 >/dev/null)& pid=$! ; sleep 0.8s ; kill -9 $pid
+        (speaker-test -t sine -f 2000 >/dev/null)& pid=$! ; sleep 0.8s ; kill -9 $pid
         sleep 0.2
     done
 fi
--- webserver.js
+++ webserver.js
@@ -17,25 +17,42 @@ function handler (req, res) { //create server
     });
 }
 
-var timevalue = "09:00";
+function makeDateTimeString(datetime) {
+    // Returns datetime string in form yyyy-MM-ddThh:mm:ss
+    yyyy = datetime.getFullYear();
+    mm = datetime.getMonth() + 1;
+    dd = datetime.getDate();
+    return yyyy + "-" + 
+        (mm > 9 ? "" : "0") + mm + "-" +
+        (dd > 9 ? "" : "0") + dd + "T" +
+        datetime.toTimeString().split(' ')[0];
+}
+
+var timevalue = makeDateTimeString(new Date());
 var alarmOn = 0;
 io.sockets.on('connection', function (socket) {// WebSocket Connection
     // Load in values from previous session
     socket.emit('alarmOn', alarmOn);
     socket.emit('console', timevalue);
 
-    socket.on('time', function(data) { //get time switch status from client
+    socket.on('time', function(data) {
         timevalue = data;
         socket.emit('console', timevalue);
     });
-    socket.on('alarmOn', function(data) { //get time switch status from client
+    socket.on('alarmOn', function(data) {
         alarmOn = data;
         socket.emit('alarmOn', alarmOn);
         socket.emit('console', timevalue);
     });
-    socket.on('button', function(data) { //get time switch status from client
+    socket.on('button', function(data) { 
+        var dt = new Date(timevalue);
+        if (dt < new Date()){
+            socket.emit('console', -1);
+            return;
+        }
         console.log("Sleep! button pressed, now running Spotify script...");
-        script = "spotify_alarm_clock " + timevalue;
+        var unixtime = dt.getTime() / 1000;
+        script = "./spotify_alarm_clock.sh " + unixtime;
         exec(script,
         (error, stdout, stderr) => {
             console.log(`${stdout}`);
